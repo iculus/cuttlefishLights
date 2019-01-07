d464229be95dd9a3d25982c22431cf98838bd6b3
iculus <mgsoroka@gmail.com>
2019-01-07 00:18:07 -0800

    this version works well, about to make changes in architecture

--- CircleFromPython/CircleFromPython.ino
+++ CircleFromPython/CircleFromPython.ino
@@ -6,10 +6,13 @@
 #define PIN 12
 #define leds 22
 #define rows 11
+#define ultrasonic A0
 
 uint16_t row = 0;
 uint16_t col = 0;
 uint16_t sz = 0;
+uint16_t finger = 0;
+uint8_t numFingers = 0;
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(leds*rows, PIN, NEO_RGB + NEO_KHZ800);
 
@@ -18,11 +21,23 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(leds*rows, PIN, NEO_RGB + NEO_KHZ800
 // and minimize distance between Arduino and first pixel.  Avoid connecting
 // on a live circuit...if you must, connect GND first.
 
+
+
+const int numReadings = 10;
+int readings[numReadings];
+int readIndex = 0;
+int total = 0;
+int average = 0;
+
 void setup() {
   Serial.begin(9600);
   
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
+
+  for (int thisReading = 0; thisReading < numReadings; thisReading++) {
+    readings[thisReading] = 0;
+  }
 }
 
 int incomingByte = 0;    // for incoming serial data
@@ -33,6 +48,7 @@ struct Message1 {
   uint8_t b; //cols
   uint8_t c; //color
   uint8_t d; //brightness
+  uint8_t e; //numFingers
 } __packed__;
 static const size_t MSG_LEN = sizeof(struct Message1);
 
@@ -41,51 +57,76 @@ void loop() {
   static char payload[MSG_LEN];
   static size_t num_payload_chars = MSG_LEN + 1;
 
-  // send data only when you receive data:
-  if (Serial.available() > 0) {
-    incomingByte = Serial.read();
-    // accumilate enough bytes
-    // look for start bytes
-    // 
-    // pull out the payload
-  
-    if (num_payload_chars == MSG_LEN + 1) {
-      if (incomingByte == 255) {
-        num_payload_chars = 0;
-      }
-    } else if (num_payload_chars < MSG_LEN) {
-      payload[num_payload_chars] = incomingByte;
-      num_payload_chars++;
-    } else if (num_payload_chars == MSG_LEN) {
-      if (incomingByte == 254) {
-        //process message
-        memcpy(&msg1, payload, sizeof(msg1));
-        handleMessage(msg1);
-        num_payload_chars = MSG_LEN + 1;
-      } else {
-        num_payload_chars = MSG_LEN + 1;  // invalid packet, drop data
-      }
-    } else {
-      //assert(false);
-    }    
+  total = total - readings[readIndex];
+  readings[readIndex] = analogRead(ultrasonic);
+  total = total + readings[readIndex];
+  readIndex = readIndex +1;
+
+  if (readIndex >= numReadings){
+    readIndex = 0;
   }
 
+  average = total/numReadings;
+  Serial.println(average);
+
+  if (average > 100){
+    row = random(0,rows);
+    col = random(0,leds);
+    sz = random(1,4+1);
+    DrawCircle(row,col,0,0,sz); // row,col,color,wait,size(1=small,4=large)
+  }
+
+  else if(average <=100){
+    // send data only when you receive data:
+    if (Serial.available() > 0) {
+      incomingByte = Serial.read();
+      // accumilate enough bytes
+      // look for start bytes
+      // 
+      // pull out the payload
+    
+      if (num_payload_chars == MSG_LEN + 1) {
+        if (incomingByte == 255) {
+          num_payload_chars = 0;
+        }
+      } else if (num_payload_chars < MSG_LEN) {
+        payload[num_payload_chars] = incomingByte;
+        num_payload_chars++;
+      } else if (num_payload_chars == MSG_LEN) {
+        if (incomingByte == 254) {
+          //process message
+          memcpy(&msg1, payload, sizeof(msg1));
+          handleMessage(msg1);
+          num_payload_chars = MSG_LEN + 1;
+        } else {
+          num_payload_chars = MSG_LEN + 1;  // invalid packet, drop data
+        }
+      } else {
+        //assert(false);
+      }    
+    }
+  }
 }
 
 void handleMessage(struct Message1 msg1){
-  Serial.print(msg1.a, DEC);
-  Serial.print(msg1.b, DEC);
-  Serial.print(msg1.c, DEC);
-  Serial.println(msg1.d, DEC);
-
+  //Serial.print(msg1.a, DEC);
+  //Serial.print(msg1.b, DEC);
+  //Serial.print(msg1.c, DEC);
+  //Serial.println(msg1.d, DEC);
+  //Serial.print(msg1.e,DEC);
   //row = random(0,rows);
   //col = random(0,leds);
   //sz = random(1,4+1);
   sz = 1;
   row = int(msg1.a);
   col = int(msg1.b);
+  finger = int(msg1.c);
+  numFingers = int(msg1.e);
 
-  DrawCircle(row,col,0,1,sz); // row,col,color,wait,size(1=small,4=large)
+  Serial.print(numFingers);
+
+  //DrawCircle(row,col,0,1,sz); // row,col,color,wait,size(1=small,4=large)
+  DrawDot(row,col,finger,1,255,numFingers); // row,col,color,wait,brightness
 }
 
 // Fill the dots one after the other with a color
@@ -235,9 +276,18 @@ void DiagonalFader(uint16_t row, uint16_t col, uint32_t c, uint8_t len, uint8_t 
 }
 
 #define ARRAY_LEN(a)    (sizeof(a) / sizeof(a[0]))
-
-uint32_t RED = strip.Color(255,0,0);
+uint32_t RED = strip.Color(0,255,0);
+uint32_t ORANGE = strip.Color(100,255,0);
 uint32_t YELLOW = strip.Color(255,255,0);
+uint32_t GREEN = strip.Color(255,0,0);
+uint32_t TEAL = strip.Color(255,0,255);
+uint32_t BLUE = strip.Color(0,0,255);
+uint32_t FUCHA = strip.Color(0,255,100);
+uint32_t PURPLE = strip.Color(0,170,255);
+uint32_t WHITE = strip.Color(255,255,255);
+uint32_t OFF = strip.Color(0,0,0);
+
+uint32_t thisColor;
 
 void DrawCircle(uint16_t row, uint16_t col, uint32_t color, uint8_t wait, uint16_t condition) {
   static int xx4[] = {-3,-3,-2,-1,0,1,2,3,3,3,2,1,0,-1,-2,-3};
@@ -258,6 +308,31 @@ void DrawCircle(uint16_t row, uint16_t col, uint32_t color, uint8_t wait, uint16
     strip.show();
     delay(wait);
   }
+}
+
+void DrawDot(uint16_t row, uint16_t col, uint32_t color, uint8_t wait, uint16_t brightness, uint8_t numFingers) {
+  static int xx1[] = {0};
+  static int yy1[] = {0};
+
+  if (numFingers == 1) {thisColor = FUCHA;}
+  if (numFingers == 5) {thisColor = PURPLE;}
+  if (numFingers == 3) {thisColor = BLUE;}
+  if (numFingers == 4) {thisColor = ORANGE;}
+  if (numFingers == 2) {thisColor = OFF;}
+  if (numFingers == 6) {thisColor = GREEN;}
+  if (numFingers == 7) {thisColor = YELLOW;}
+  
+  addressShape(xx1,yy1,row,col,ARRAY_LEN(xx1),thisColor);
+  strip.setBrightness(brightness);
+  strip.show();
+  delay(wait);
+  if (numFingers == 10) {
+    for(int i=0; i< strip.numPixels(); i++) {
+      strip.setPixelColor(i, strip.Color(0,0,0));
+    }  
+  }
+  strip.show();
+  delay(wait);
 }
 
 void addressShape(int* xArray, int* yArray, uint16_t thisrow, uint16_t thiscol, uint16_t arraylen,uint32_t c){
--- leapController/leapcontroller.py
+++ leapController/leapcontroller.py
@@ -1,6 +1,93 @@
-import serial, time, random, struct
+#!/usr/bin/python
 
-ser = serial.Serial(port = '/dev/ttyACM0', baudrate = 9600,timeout = 0)
+import os, sys, inspect, thread, time
+		
+LIB_DIR = "/home/mike/Desktop/LeapDeveloperKit_2.3.1+31549_linux/LeapSDK/lib"
+
+src_dir = os.path.dirname(inspect.getfile(inspect.currentframe()))
+arch_dir = '/x64' if sys.maxsize > 2**32 else '/x86'
+
+sys.path.insert(0, os.path.abspath(os.path.join(src_dir, LIB_DIR + arch_dir)))
+
+sys.path.insert(0, LIB_DIR)
+
+import Leap 
+from numpy import interp
+
+LeapRowMin = -140
+LeapRowMax = 140
+M0RowMin = 11
+M0RowMax = 0
+LeapColMin = 50
+LeapColMax = 400
+M0ColMin = 0
+M0ColMax = 22
+
+class SampleListener(Leap.Listener):
+
+	def on_connect(self,controller):
+		print "connected"
+
+	def on_frame(self, controller):
+		extended_fingers = controller.frame().fingers.extended()
+		numFing = len(extended_fingers)
+		for f in extended_fingers:
+		
+			thisRow = f.stabilized_tip_position[0]
+			thisRow = clamp(thisRow,LeapRowMin,LeapRowMax)
+			thisRow = int( interp(thisRow,[LeapRowMin,LeapRowMax],[M0RowMin,M0RowMax]) )
+			
+			thisCol = f.stabilized_tip_position[1]
+			thisCol = clamp(thisCol,LeapColMin,LeapColMax)
+			thisCol = int( interp(thisCol,[LeapColMin,LeapColMax],[M0ColMin,M0ColMax]) )
+			print f.touch_distance, f.type, thisRow, thisCol, numFing
+			#print readSerial()
+			
+			#build struct and send message
+			start = chr(255)
+			end = chr(254)
+
+			x = thisRow #rows
+			y = thisCol #columns
+			fing = f.type #controls color
+			depth = random.randrange(10) #controls brightness
+
+			message = start + struct.pack("<BBBBB", x,y,fing,depth,numFing) + end
+
+			ser.write(message)
+			
+			#print numFing
+
+			#print 'writing', time.time()
+			
+def clamp(n, minn, maxn):
+    return max(min(maxn, n), minn)
+
+def readSerial():
+	for line in ser.read():
+		return line
+
+def main():
+	listener = SampleListener()
+	controller = Leap.Controller()
+
+	controller.add_listener(listener)
+
+	# Keep this process running until Enter is pressed
+	print "Press Enter to quit..."
+	try:
+		sys.stdin.readline()
+	except KeyboardInterrupt:
+		pass
+	finally:
+		controller.remove_listener(listener)
+
+import serial, random, struct
+
+try:
+	ser = serial.Serial(port = '/dev/ttyACM1', baudrate = 9600,timeout = 0)
+except:
+	ser = serial.Serial(port = '/dev/ttyACM0', baudrate = 9600,timeout = 0)
 
 try:
 	print 'its open'
@@ -14,28 +101,5 @@ except IOError: # if port is already opened, close it and open it again and prin
 	ser.close()
 	ser.open()
 
-def readSerialPort():
-	for line in ser:
-		return line	
-
-
 if __name__ == "__main__":
-
-	while 1:
-		while ser.inWaiting() > 0:
-			ser.read(1)
-	
-		start = chr(255)
-		end = chr(254)
-	
-		x = random.randrange(11) #rows
-		y = random.randrange(22) #columns
-		fing = random.randrange(10) #controls color
-		depth = random.randrange(10) #controls brightness
-	
-		message = start + struct.pack("<BBBB", x,y,fing,depth) + end
-
-		ser.write(message)
-	
-		print 'writing', time.time()
-		print readSerialPort()
+	main()
